d69d8090ebed68b7a43986c1f1d796ff4e08ca34
patrykxk <patrix215@gmail.com>
2017-04-04 10:20:58 +0200

    Modyfikacja klasy FakeTime

--- src/main/java/edu/iis/mto/time/FakeTime.java
+++ src/main/java/edu/iis/mto/time/FakeTime.java
@@ -4,9 +4,13 @@ import org.joda.time.DateTime;
 
 
 public class FakeTime {
+    private static final long TWENTY_FIVE_HOURS = 25*60*60*1000;
 
-    public int getHours(){
-        return  new DateTime().getHourOfDay()+25;
+    public long currentTimeMillis(){
+        return  System.currentTimeMillis() + TWENTY_FIVE_HOURS;
+    }
+    public int hoursBetweenMs(long start, long end ){
+        return (int)(end-start)/(60*60*1000);
     }
 
 }
