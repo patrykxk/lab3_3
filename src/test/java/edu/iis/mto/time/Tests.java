f4cea4348892ab870e7fbcc5da0d0975bf4527ea
patrykxk <patrix215@gmail.com>
2017-04-04 10:31:19 +0200

    Dodanie testu sprawdzającego wyrzucenie wyjątku OrderExpired

--- src/test/java/edu/iis/mto/time/Tests.java
+++ src/test/java/edu/iis/mto/time/Tests.java
@@ -1,5 +1,14 @@
 package edu.iis.mto.time;
 
 
+import org.junit.Test;
+
 public class Tests {
+
+    @Test(expected = OrderExpiredException.class)
+    public void orderExpired(){
+        Order order = new Order();
+        order.submit();
+        order.confirm();
+    }
 }
